7336c304633b2b4ce3ff617c89115ba7d395d94d
irinataran17 <irina.taran17@gmail.com>
2019-01-20 03:34:11 +0200

    Add POST and GET /employees routes, as well GET route for

--- server/server.js
+++ server/server.js
@@ -30,6 +30,44 @@ app.get('/departments', async (req, res) => {
     } catch (e) {
         res.status(400).send(e);
     }
+});
+
+app.post('/employees', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ['name', 'jobTitle', 'email', 'department']);
+        const employee = new Employee(body);
+        await employee.save();
+        res.send(employee);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+app.get('/employees', async (req, res) => {
+    try {
+        const employees = await Employee.find();
+        res.send(employees);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+app.get('/department/:id', async (req, res) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+    try {
+        const requestedDep = await Department.findOne({
+            _id: id
+        });
+        const employees = await Employee.find({
+            department: requestedDep.name
+        });
+        res.send(employees);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 })
 
 
